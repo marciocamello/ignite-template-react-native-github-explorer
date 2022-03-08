746944876208126e73328d043dedee75ccd49d08
marciocamello <marciocamello@oulook.com>
2022-03-08 18:25:32 +0000

    Finish challenge

--- src/components/Card/CardAnimation/index.tsx
+++ src/components/Card/CardAnimation/index.tsx
@@ -1,38 +1,46 @@
 import React, { useEffect } from 'react';
 import { useWindowDimensions, ViewProps } from 'react-native';
 import {
-  useAnimatedStyle,
-  useSharedValue,
-  withTiming
+    useAnimatedStyle,
+    useSharedValue,
+    withTiming
 } from 'react-native-reanimated';
 
 import { AnimationContainer } from './styles';
 
 interface CardAnimationProps extends ViewProps {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }
 
 export function CardAnimation({ children, ...rest }: CardAnimationProps) {
-  const { width: displayWidth } = useWindowDimensions();
-  const cardOpacity = useSharedValue(0);
-  const cardOffset = useSharedValue(0.25 * displayWidth);
+    const { width: displayWidth } = useWindowDimensions();
+    const cardOpacity = useSharedValue(0);
+    const cardOffset = useSharedValue(0.25 * displayWidth);
 
-  const animatedStyle = useAnimatedStyle(() => {
-    return {
-      // TODO - setup animated style
-    }
-  })
+    const animatedStyle = useAnimatedStyle(() => {
+        return {
+            // TODO - setup animated style
+            opacity: cardOpacity.value,
+            transform: [
+                {
+                    translateX: cardOffset.value
+                }
+            ]
+        }
+    })
 
-  useEffect(() => {
-    /**
-     * TODO - setup cardOpacity.value and cardOffset.value with
-     * withTiming()
-     */
-  }, []);
+    useEffect(() => {
+        /**
+         * TODO - setup cardOpacity.value and cardOffset.value with
+         * withTiming()
+         */
+        cardOpacity.value = withTiming(1, { duration: 1000 });
+        cardOffset.value = withTiming(0 * displayWidth, { duration: 1000 });
+    }, []);
 
-  return (
-    <AnimationContainer {...rest} style={animatedStyle}>
-      {children}
-    </AnimationContainer>
-  )
+    return (
+        <AnimationContainer {...rest} style={animatedStyle}>
+            {children}
+        </AnimationContainer>
+    )
 }
